fb6b67e1762a7de2bd9dc7e047fdd04fe9bf4755
Carlos Henrique <ch.henriquevinha@gmail.com>
2022-10-05 12:59:39 -0300

    add config emacs

---
+++ emacs/.emacs.d/elpa/rebecca-theme-20180324.821/rebecca-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from rebecca-theme-20180324.821/rebecca-theme.el  -*- no-byte-compile: t -*-
+(define-package "rebecca-theme" "20180324.821" "Rebecca Purple Theme" '((emacs "24")) :commit "1fe3662d1b02caea96e9a780252b2c45f7a49b1d" :authors '(("vic" . "vborja@apache.org")) :maintainer '("vic" . "vborja@apache.org") :keywords '("theme" "dark") :url "https://github.com/vic/rebecca-theme")
