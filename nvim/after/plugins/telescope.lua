a530a19dda9937ea05d91e64d52b5fe4e8af3810
Carlos Henrique <ch.henriquevinha@gmail.com>
2023-06-27 17:45:17 +0000

    change theme telescope

--- nvim/after/plugins/telescope.lua
+++ nvim/after/plugins/telescope.lua
@@ -1,7 +1,19 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+local themes = require('telescope.themes')
+
+
+local custom_find_files = function()
+  local theme = themes.get_ivy {
+    -- Customize theme options here if needed
+    previewer = false, -- Desabilitar o preview apenas para find_files
+  }
+  builtin.find_files(theme)
+end
+
+vim.keymap.set('n', '<leader>f', custom_find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+
