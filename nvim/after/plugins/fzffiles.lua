49f2c48d230e5b40f60c7a4eb278b957400a1ac6
hsolrac <ch.henriquevinha@gmail.com>
2024-02-15 10:22:42 -0300

    nvim: update lsp

--- nvim/after/plugins/fzffiles.lua
+++ nvim/after/plugins/fzffiles.lua
@@ -12,3 +12,4 @@ fzf.setup({"telescope",
 vim.keymap.set({ "n" }, "<C-p>", "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
 vim.keymap.set("n", "<c-f>", "<cmd>lua require('fzf-lua').grep()<CR>", { silent = true })
 vim.keymap.set("n", "<c-g>", "<cmd>lua require('fzf-lua').git_branches()<CR>", { silent = true })
+vim.keymap.set("n", "<c-b>", "<cmd>lua require('fzf-lua').buffers()<CR>", { silent = true })
