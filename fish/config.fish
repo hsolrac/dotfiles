7cf167ce9bab5e72ca9c2345cb669bd732b2d4cb
hsolrac <ch.henriquevinha@gmail.com>
2024-04-09 16:10:35 -0300

    Atualiza preferencias i3wm

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,6 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
 source ~/.asdf/asdf.fish
 
 export JAVA_HOME=/home/carlos/.asdf/installs/java/openjdk-17.0.1
-export JIRA_API_TOKEN="ATATT3xFfGF00DI76XguFYNYIJMFs_wGNL9j5KY3qE5ef27uCpMBz_Aj5K2btmuI21OI0nrpf3nveyhcYPPGH9d2V9j9XYsqGB1GuN_7Dn5RQLA2J33L1PkaVv4ljy_fySPHTkhp_47FKgQv7jWObwGDXqIt5lguUQLAbL0ogUJiuWeHV07pdm4=9E959492"
-export FLYCTL_INSTALL="/home/carlos/.fly"
 export PATH="$FLYCTL_INSTALL/bin:$PATH"
 # ALIAS
 
