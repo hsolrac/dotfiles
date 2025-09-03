4c9050b8370d12f026a55cc050578a9331152bbb
Carlos Henrique <ch.henriquevinha@gmail.com>
2025-09-02 21:45:23 -0300

    Add configs nixos

---
+++ nix-os/flakes/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "node + mongo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+					config = {
+						allowUnfree = true;
+          	allowUnfreePredicate = _: true;
+					};
+        };
+				
+
+        nodejs = pkgs.nodejs_20;  
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            nodejs
+						pkgs.pnpm
+						pkgs.awscli2
+						pkgs.git
+						pkgs.mongosh
+          ];
+
+          shellHook = ''
+            echo "Dev environment ready"
+          '';
+        };
+      }
+    );
+}
+
