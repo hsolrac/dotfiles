5704abf384b28dfc7f61b53f2cdae8cbe1574149
Carlos Henrique <ch.henriquevinha@gmail.com>
2025-09-22 14:47:51 -0300

    Add nvim nightly config

--- nix-os/configuration.nix
+++ nix-os/configuration.nix
@@ -1,28 +1,17 @@
-# Edi this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "nixos";
   networking.extraHosts = ''
-     127.0.0.1 local.locavibe.com.br
+    127.0.0.1 local.locavibe.com.br
   '';
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   fonts.packages = with pkgs; [ 
     nerd-fonts.fira-code
@@ -30,20 +19,17 @@
   ];
 
   nix = {
-    package = pkgs.nix;  # optional but recommended
+    package = pkgs.nix;
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
   };
 
   fonts.fontconfig.enable = true;
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/Sao_Paulo";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "pt_BR.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -58,8 +44,6 @@
     LC_TIME = "pt_BR.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
   environment.pathsToLink = [ "/libexec" ]; 
 
   services.xserver = {
@@ -71,7 +55,6 @@
   
     displayManager = {
       lightdm.enable = true;
-      defaultSession = "none+i3";
     };
   
     windowManager.i3 = {
@@ -85,6 +68,9 @@
       ];
     };
   };
+
+  services.displayManager.defaultSession = "none+i3";
+  
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
 
@@ -92,20 +78,15 @@
     Option "PreferredMode" "1920x1080"
   '';
 
-
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "br";
     variant = "thinkpad";
   };
 
-  # Configure console keymap
   console.keyMap = "br-abnt2";
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -113,40 +94,41 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.carlos = {
     isNormalUser = true;
     description = "carlos";
     extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [
-    #  thunderbird
     ];
     shell = pkgs.fish;
   };
+  
   virtualisation.docker.enable = true;
-  # Install firefox.
   programs.firefox.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config = {
+   packageOverrides = pkgs: let
+      pkgs' = import <nixpkgs-unstable> {
+        inherit (pkgs) system;
+        overlays = [
+          (import (builtins.fetchTarball {
+            url = "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz";
+          }))
+        ];
+      };
+    in {
+      inherit (pkgs') neovim;
+    };
+  };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
+    neovim
     wget
-    neovim 
     git 
     unzip 
     gzip 
@@ -173,31 +155,6 @@
     feh
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
   programs.fish.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
-
+  system.stateVersion = "25.05";
 }
