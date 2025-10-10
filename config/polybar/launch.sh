abdd227dbb60fe4ac93198cf54923be3e5f608cc
Carlos Henrique <ch.henriquevinha@gmail.com>
2025-10-10 10:27:07 -0300

    Add picom and rofi

---
+++ config/polybar/launch.sh
@@ -0,0 +1,9 @@
+pkill -x polybar
+while pgrep -x polybar >/dev/null; do sleep 1; done
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload minimal &
+  done
+else
+  polybar --reload minimal &
+fi
