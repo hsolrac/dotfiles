411fba0a1dcf74b38f7755dffe76689a644290e1
Carlos Henrique <ch.henriquevinha@gmail.com>
2025-10-28 19:34:28 -0300

    fix nix flake configuration

---
+++ nixcfg/flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "Flake configuration of Hsolrac";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+  outputs = { self, nixpkgs }: {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+          modules = [ ./configuration.nix ];
+    };
+  };
+}
